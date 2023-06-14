91ae6fe1e3f707c90a7af23b53643cb3cb28a1ae
kssmnw <113131859+kssmnw@users.noreply.github.com>
2023-06-14 20:59:26 +0300

    Update func.h

--- 15 puzzle/func.h
+++ 15 puzzle/func.h
@@ -1,1 +1,29 @@
+#include <iostream>
+#define MAX_SIZE 50
+
+struct Square {
+  char data[MAX_SIZE][MAX_SIZE];
+};
+
+bool load_image(const std::string &filename, char (&image)[512][512], 
+                 int *x, int *y);
+
+void divide_image(const char (&image)[512][512], Square *squares, 
+                  int sx, int sy, int square_count, int square_size);
+
+int frame(int count);
+
+void draw_squares(Square *squares, int rows, int columns, 
+                  int square_size, int x, int y);
+
+int change(Square *squares, int square_count, int square_size, 
+           int a, int b);
+int step(Square *squares, int rows, int columns, int square_size, 
+         int *x, int *y, int input);
+
+void game(Square *squares, int rows, int columns, int square_size, 
+          int *x, int *y);
+
+void scrumble(Square *squares, int rows, int columns, int square_size,
+             int *x, int *y);
 
